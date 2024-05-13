794b4d811c54fe8d2d30087d2f1aaf2eca7b5bf5
korede90 <koredeoyebamiji68@gmail.com>
2024-05-13 13:10:12 +0100

    updated app.py

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ with st.sidebar:
                          ("Normal", "Hd"))
     thresh = st.slider("Threshold", min_value = 0.40, max_value = 0.99)
 with st.expander('About this app'):
-    st.text('This app was created on this day')
+    st.text('This app is only available for images')
     
     
 # button upload
-img_file = st.file_uploader("Blood upload your image", type = ['png', 'jpg'], help = "This should only be images useless human being cos y'all won't listen to instructions.")
+img_file = st.file_uploader("Blood upload your image", type = ['png', 'jpg'], help = "Only upload an image file.")
 
 # img upload and save
 if img_file:
     col1, col2 = st.columns(2)
-    col1.image(img_file, caption = "This is your image bro", use_column_width = True)
+    col1.image(img_file, caption = "Image Uploaded", use_column_width = True)
     # st.image(img_file, caption = "This is your image bro")
     Image.open(img_file).save(img_file.name)
     results = model(img_file.name, stream = False)
     results[0].save(filename = 'result.jpg')
-    col2.image('result.jpg', caption = 'This is your Output')
+    col2.image('result.jpg', caption = 'Image output')
